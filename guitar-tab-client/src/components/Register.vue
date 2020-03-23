ad97c20287a32cc338ad537ac1c2f5efe7e04c59
ahmedfadhil <ahmed.fadhil@alumni.emu.edu.tr>
2020-03-23 13:08:50 +0100

    added the server side

---
+++ guitar-tab-client/src/components/Register.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input type="email" v-model="email" name="email" placeholder="email" />
+    <br />
+    <input type="password" v-model="password" name="password" placeholder="password" />
+    <br />
+    <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from "../services/AuthenticationService";
+export default {
+  name: "register",
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    async register() {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      });
+      console.log(response.data);
+    }
+  }
+  //   watch: {
+  //     email(value) {
+  //       console.log("email has changed", value);
+  //     }
+  //   },
+  //   mounted() {
+  //     setTimeout(() => {
+  //       this.email = "hello world";
+  //     }, 2000);
+  //   }
+};
+</script>
+<style scoped>
+</style>
